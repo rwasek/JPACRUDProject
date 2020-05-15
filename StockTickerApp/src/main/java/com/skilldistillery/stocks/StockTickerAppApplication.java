536a08c7c3ccb530e6b2ff6d3a7627c3d7ef024d
Rich Wasek <rideburtonrw@gmail.com>
2020-05-15 17:37:39 -0600

    Stub Boot/MVC project, connect JPA project dependency

---
+++ StockTickerApp/src/main/java/com/skilldistillery/stocks/StockTickerAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTickerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTickerAppApplication.class, args);
+	}
+
+}
---
+++ StockTickerApp/src/test/java/com/skilldistillery/stocks/StockTickerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTickerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
